98b398a9a623ad47180053f74b2e126118c6406a
Dhanya <dhanya@dell.com>
2025-02-26 22:39:58 +0530

    re structured Frontend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, "../FRONTEND/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../FRONTEND/dist", "index.html"));
 });
- 
+  
 const port = process.env.PORT
 // Consider making DB connection before starting server
 const startServer = async () => {
